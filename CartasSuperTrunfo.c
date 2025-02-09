6547e0af344e515a51c396ea27a032fd46bbd6bf
ByteMe <cindyarkan7@gmail.com>
2025-02-09 18:27:03 +0000

    colocquei o pprinf e o scanf para o conseguir cadrastar o super tunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    
     printf ("Super trunfo! \n");
 
-    char Nome[50], codigo[10], PIB [50];
-    float area;
+    char Nome[50];
+   float area;
     int turismo;
+    float PIB;
+    char codigo[10];
     
     printf("Digite o nome da cidade: \n");
     fgets(Nome, sizeof(Nome), stdin);
@@ -17,22 +18,23 @@ int main() {
     printf("Digite o número de pontos turisticos: \n");
     scanf("%d", &turismo);
 
-    printf("Digite o PIB: \n");
-    scanf("%s", &PIB);
+    printf("Digite o PIB:\n");
+    scanf("%f", &PIB);
 
     printf("Digite o codigo da cidade: \n");
     scanf("%s", &codigo);
 
-    printf("Nome da cidade: %s", Nome);
-    printf("Área: %.2f km²\n", area);
+     printf("Nome da cidade: %s", Nome);
+     printf("Área: %.2f km²\n", area);
     printf("Número de pontos turísticos: %d\n", turismo);
-    printf("Número do PIB: %s\n", PIB);
+    printf("Número do PIB: %f\n", PIB);
     printf("Código da cidade: %s", codigo);
     
 
     return 0;
 
 }
+
 
 
 
